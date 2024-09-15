88db9596584612d3851e2b3dbf676b57ddce8540
tvoelkner <adm_t.voelkner@baustoffe-wolf.de>
2024-09-15 15:36:36 +0200

    Add files via upload

---
+++ Get_Fslogix_Redirectionsfile_Configuration.ps1
@@ -0,0 +1,21 @@
+﻿# Quellpfad der Datei
+$source = "C:\Temp\redirections.xml"
+
+# Zielpfad im Benutzerprofil
+$destination = "$env:USERPROFILE\AppData\Local\FSLogix\redirections.xml"
+
+# Erstellen des Zielverzeichnisses, falls es nicht existiert
+$destinationDir = [System.IO.Path]::GetDirectoryName($destination)
+if (-not (Test-Path -Path $destinationDir)) {
+    New-Item -ItemType Directory -Path $destinationDir -Force
+}
+
+# Kopieren der Datei
+Copy-Item -Path $source -Destination $destination -Force
+
+# Bestätigung der Dateiablage
+if (Test-Path -Path $destination) {
+    Write-Output "Die Datei wurde erfolgreich nach $destination kopiert."
+} else {
+    Write-Output "Fehler beim Kopieren der Datei."
+}
---
+++ Set_Core_Fslogix_Redirectionsfile_Configuration.ps1
@@ -0,0 +1,20 @@
+﻿# URL der Datei auf GitHub
+$url = "https://raw.githubusercontent.com/tvoelkner/avdpowershell/main/redirections.xml"
+
+# Zielpfad auf dem lokalen System
+$destination = "C:\temp\redirections.xml"
+
+# Erstellen des Zielverzeichnisses, falls es nicht existiert
+if (-not (Test-Path -Path "C:\temp")) {
+    New-Item -ItemType Directory -Path "C:\temp"
+}
+
+# Herunterladen der Datei
+Invoke-WebRequest -Uri $url -OutFile $destination
+
+# Bestätigung der Dateiablage
+if (Test-Path -Path $destination) {
+    Write-Output "Die Datei wurde erfolgreich nach C:\temp heruntergeladen."
+} else {
+    Write-Output "Fehler beim Herunterladen der Datei."
+}
